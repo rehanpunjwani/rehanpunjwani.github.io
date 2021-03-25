682f183babbb01d571e04d33773094df818036fc
Rehan-Rehman-Punjwani <49568477+Rehan-Rehman-Punjwani@users.noreply.github.com>
2021-03-26 03:14:39 +0500

    initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,530 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Rehan's Portfolio",
+  description:
+    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  og: {
+    title: "Rehan's Portfolio",
+    type: "website",
+    url: "http://rehanpunjwani.github.io/",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Rehan Rehman",
+  logo_name: "Punjwani Rehan",
+  nickname: "rehanpunjwani",
+  subTitle:
+    "A passionate, technophile who always thrive to work on end to end products which develop sustainable and scalable technical systems to create impact.One who always look out for common questions and solving them using emerging technologies.",
+  resumeLink:
+    "https://drive.google.com/file/d/17x-QIccVR4OXxkfE8RAa9BQbYe2mzTEP/view?usp=sharing",
+  portfolio_repository: "",
+};
+
+const socialMediaLinks = [
+  /* Your Social Media Link */
+  // github: "https://github.com/ashutosh1919",
+  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+  // gmail: "ashutoshhathidara98@gmail.com",
+  // gitlab: "https://gitlab.com/ashutoshhathidara98",
+  // facebook: "https://www.facebook.com/laymanbrother.19/",
+  // twitter: "https://twitter.com/ashutosh_1919",
+  // instagram: "https://www.instagram.com/layman_brother/"
+
+  {
+    name: "Github",
+    link: "https://github.com/rehanpunjwani",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/rehan-rehman-punjwani/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+  {
+    name: "Gmail",
+    link: "mailto:rehanrehman.2016404@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+];
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building resposive website front end using NextJS, ReactJS and ContextAPI's",
+        "⚡ Developing mobile applications using React Native and solo android apps using Kotlin",
+        "⚡ Creating application backend in Node, Express & Flask",
+        "⚡ Wordpress Installation to dynamic websites in minutes",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "NextJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Yarn",
+          fontAwesomeClassname: "simple-icons:yarn",
+          style: {
+            color: "#2C8EBB",
+          },
+        },
+        {
+          skillName: "SpringBoot",
+          fontAwesomeClassname: "simple-icons:spring",
+          style: {
+            color: "#28a745",
+          },
+        },
+        {
+          skillName: "worpdress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#02569B",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases",
+        "⚡ Deploying deep learning models on cloud to use on mobile devices",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "Kubernetes",
+          fontAwesomeClassname: "simple-icons:kubernetes",
+          style: {
+            color: "#326CE5",
+          },
+        },
+      ],
+    },
+    {
+      title: "Data Science & AI",
+      fileName: "DataScienceImg",
+      skills: [
+        "⚡ Developing highly scalable production ready models for various deeplearning and statistical use cases",
+        "⚡ Experience of working with Computer Vision and NLP projects",
+        "⚡ Complex quantitative modelling for dynamic forecasting and time series analysis",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Tensorflow",
+          fontAwesomeClassname: "logos-tensorflow",
+          style: {
+            backgroundColor: "transparent",
+          },
+        },
+        {
+          skillName: "Keras",
+          fontAwesomeClassname: "simple-icons:keras",
+          style: {
+            backgroundColor: "white",
+            color: "#D00000",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "ion-logo-python",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "HackerRank",
+      iconifyClassname: "simple-icons:hackerrank",
+      style: {
+        color: "#2EC866",
+      },
+      profileLink: "https://www.hackerrank.com/rehanpunjwani",
+    },
+    {
+      siteName: "Codechef",
+      iconifyClassname: "simple-icons:codechef",
+      style: {
+        color: "#5B4638",
+      },
+      profileLink: "https://www.codechef.com/users/rehanpunjwani",
+    },
+    {
+      siteName: "Codeforces",
+      iconifyClassname: "simple-icons:codeforces",
+      style: {
+        color: "#1F8ACB",
+      },
+      profileLink: "http://codeforces.com/profile/rehanpunjwani",
+    },
+    {
+      siteName: "Kaggle",
+      iconifyClassname: "simple-icons:kaggle",
+      style: {
+        color: "#20BEFF",
+      },
+      profileLink: "https://www.kaggle.com/rehanpunjwani",
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Ghulam Ishaq Khan Institute of Engineering and Technology",
+      subtitle: "B.S in Computer Engineering",
+      logo_path: "giki.png",
+      alt_name: "GIKI",
+      duration: "2016 - Present",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
+        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+      ],
+      website_link: "http://www.giki.edu.pk",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Machine Learning",
+      subtitle: "- Andrew Ng",
+      logo_path: "stanford_logo.png",
+      alt_name: "Stanford University",
+      color_code: "#8C151599",
+    },
+    {
+      title: "Algorithms",
+      subtitle: "- Tim Roughgarden",
+      logo_path: "stanford_logo.png",
+      certificate_link: "http://coursera.org/verify/RPHT47BTVQPM",
+      alt_name: "Stanford University",
+      color_code: "#8C151599",
+    },
+    {
+      title: "Deep Learning",
+      subtitle: "- Andrew Ng",
+      logo_path: "deeplearning_ai_logo.png",
+      alt_name: "deeplearning.ai",
+      color_code: "#00000099",
+    },
+    {
+      title: "ML on GCP",
+      subtitle: "- GCP Training",
+      logo_path: "google_logo.png",
+      alt_name: "Google",
+      color_code: "#0C9D5899",
+    },
+    {
+      title: "Data Science",
+      subtitle: "- Alex Aklson",
+      logo_path: "ibm_logo.png",
+      alt_name: "IBM",
+      color_code: "#1F70C199",
+    },
+    {
+      title: "Deep Learning Explained",
+      subtitle: "- Karen Kocher ",
+      logo_path: "microsoft_logo.png",
+      certificate_link:
+        "https://courses.edx.org/certificates/6aa96847b715447495ff3747b97bd512",
+      alt_name: "Microsoft",
+      color_code: "#D83B0199",
+    },
+    {
+      title: "Advanced Data Science",
+      subtitle: "- Romeo Kienzler",
+      logo_path: "ibm_logo.png",
+      alt_name: "IBM",
+      color_code: "#1F70C199",
+    },
+    {
+      title: "Advanced ML on GCP",
+      subtitle: "- GCP Training",
+      logo_path: "google_logo.png",
+      alt_name: "Google",
+      color_code: "#0C9D5899",
+    },
+    {
+      title: "Fullstack Development",
+      subtitle: "- Stephen Grider",
+      logo_path: "udemy_logo.png",
+      alt_name: "Udemy",
+      color_code: "#2A73CC",
+    },
+    {
+      title: "Kuberenetes on GCP",
+      subtitle: "- Qwiklabs",
+      logo_path: "gcp_logo.png",
+      certificate_link:
+        "https://google.qwiklabs.com/public_profiles/e4d5a92b-faf6-4679-a70b-a9047c0cd750",
+      alt_name: "GCP",
+      color_code: "#4285F499",
+    },
+    {
+      title: "Advanced ML: ML Infrastructure",
+      subtitle: "- Qwiklabs",
+      logo_path: "gcp_logo.png",
+      certificate_link:
+        "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
+      alt_name: "GCP",
+      color_code: "#4285F499",
+    },
+    {
+      title: "Data Science on Google Cloud:Machine Learning",
+      subtitle: "- Qwiklabs",
+      logo_path: "gcp_logo.png",
+      certificate_link:
+        "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
+      alt_name: "GCP",
+      color_code: "#4285F499",
+    },
+    {
+      title: "BigQuery for Marketing Analysts",
+      subtitle: "- Qwiklabs",
+      logo_path: "gcp_logo.png",
+      certificate_link:
+        "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
+      alt_name: "GCP",
+      color_code: "#4285F499",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I have been working as a Full-stack engineer in a startup funded by big VC in california. I have also interned with some well established companies as AI and Web Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      experiences: [
+        {
+          title: "Full-Stack Engineer",
+          company: "XCUTE.INC",
+          company_url: "https://xcute.co/",
+          logo_path: "legato_logo.png",
+          duration: "Aug 2020 - PRESENT",
+          location: "Arlington - Delaware",
+          description:
+            "I am part of the founding team of a new freelancing marketplace, first of its kind in Middle East and North Africa. My role is to lead the team of other engineers to create scalable microservice based web application",
+          color: "#0879bf",
+        },
+      ],
+    },
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Software Development Intern",
+          company: "Techlogix.Inc",
+          company_url: "https://www.linkedin.com/company/techlogix/",
+          logo_path: "delhivery_logo.png",
+          duration: "Jun 2019 - August 2019",
+          location: "Karachi, Pakistan",
+          description: [
+            "• Worked on Front-end Technologies like Angular 8, ReactJs, Bootstrap and Javascript",
+            "              • Developed APIs for Auto Generation of Reports and Emails using Selenium, Spring-Boot Framework, and MailGun API",
+            "              • Design and Testing of responsive Template of Email using Litmus Email Tool",
+          ],
+          color: "#ee3c26",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Microsoft Student Partner",
+          company: "Microsoft",
+          company_url: "https://www.microsoft.com/",
+          logo_path: "microsoft_logo.png",
+          duration: "Aug 2019 - May 2020",
+          location: "Swabi, Topi",
+          description:
+            "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
+          color: "#D83B01",
+        },
+        {
+          title: "Mozilla Campus Captain",
+          company: "Mozilla",
+          company_url: "https://www.mozilla.org/",
+          logo_path: "mozilla_logo.png",
+          duration: "Oct 2019 - May 2020",
+          location: "Topi, Swabi",
+          description:
+            "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
+          color: "#000000",
+        },
+        {
+          title: "Developer Students Club Member",
+          company: "DSC GIKI",
+          company_url: "https://www.linkedin.com/company/googledevelopers/",
+          logo_path: "dsc_logo.png",
+          duration: "Jan 2018 - May 2020",
+          location: "Topi, Swabi",
+          description:
+            "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
+          color: "#0C9D58",
+        },
+        {
+          title: "Developer Program Member",
+          company: "Github",
+          company_url: "https://github.com/",
+          logo_path: "github_logo.png",
+          duration: "July 2019 - PRESENT",
+          location: "Work From Home",
+          description:
+            "I am actively contributing to many opensource projects. I have contributed to projects of organisations like Tensorflow, Uber, Facebook, Google, Scikit-learn, Kiwix, Sympy, Python, NVLabs, Fossasia, Netrack, Keras etc. These contributions include bug fixes, feature requests and formulating proper documentation for project.",
+          color: "#181717",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "animated_ashutosh.png",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+  },
+  blogSection: {},
+  addressSection: {
+    title: "Address",
+    subtitle:
+      "Ambavadi vas, Kanodar, T.A.-Palanpur, Dist.-Banaskantha, Gujarat - 385520",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "https://goo.gl/maps/MpMqtRZytFchMkZ76",
+  },
+  phoneSection: {
+    title: "Phone Number",
+    subtitle: "+91 8320758513",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+};
